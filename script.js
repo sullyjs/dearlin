36efb81247b2df83800b2917ac06a92236f411b1
SJ <jsylvir@gmail.com>
2026-07-04 23:53:34 +0200

    Implement Scene class and clear console
    Added Scene class and utility function for property definition.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} // clearing the console (just a CodePen thing)
+
+console.clear();
+
+// there are 3 parts to this
+//
+// Scene:           Setups and manages threejs rendering
+// loadModel:       Loads the 3d obj file
+// setupAnimation:  Creates all the GSAP 
+//                  animtions and scroll triggers 
+//
+// first we call loadModel, once complete we call
+// setupAnimation which creates a new Scene
+
+class Scene
+{
+  constructor(model)
+  {_defineProperty(this, "render",
